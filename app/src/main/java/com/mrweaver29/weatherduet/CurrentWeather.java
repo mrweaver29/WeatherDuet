6e34d6b85dc820542055309a2b26202db36690c1
mrweaver29 <mrweaver29@outlook.com>
2015-04-24 19:51:06 -0400

    update CurrentWeather.java

--- app/src/main/java/com/mrweaver29/weatherduet/CurrentWeather.java
+++ app/src/main/java/com/mrweaver29/weatherduet/CurrentWeather.java
@@ -1,5 +1,9 @@
 package com.mrweaver29.weatherduet;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.TimeZone;
+
 /**
  * Created by Benjamin on 4/24/2015.
  */
@@ -9,8 +13,17 @@ public class CurrentWeather {
     private long mTime;
     private double mTemperature;
     private double mHumidity;
-    private double mPrecipitation;
+    private double mPrecipChance;
     private String mSummary;
+    private String mTimeZone;
+
+    public String getTimeZone() {
+        return mTimeZone;
+    }
+
+    public void setTimeZone(String timeZone) {
+        mTimeZone = timeZone;
+    }
 
     public String getLocation() {
         return mLocation;
@@ -30,6 +43,15 @@ public class CurrentWeather {
 
     public long getTime() {
         return mTime;
+    }
+
+    public String getFormattedTime() {
+        SimpleDateFormat formatter = new SimpleDateFormat("h:mm a");
+        formatter.setTimeZone(TimeZone.getTimeZone(getTimeZone()));
+        Date dateTime = new Date(getTime() * 1000);
+        String timeString = formatter.format(dateTime);
+
+        return timeString;
     }
 
     public void setTime(long time) {
@@ -52,12 +74,12 @@ public class CurrentWeather {
         mHumidity = humidity;
     }
 
-    public double getPrecipitation() {
-        return mPrecipitation;
+    public double getPrecipChance() {
+        return mPrecipChance;
     }
 
-    public void setPrecipitation(double precipitation) {
-        mPrecipitation = precipitation;
+    public void setPrecipChance(double precipChance) {
+        mPrecipChance = precipChance;
     }
 
     public String getSummary() {
