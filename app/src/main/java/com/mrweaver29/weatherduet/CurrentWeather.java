825a9b4d88b661be20e3bb5e035d2e6f576a0cb5
mrweaver29 <mrweaver29@outlook.com>
2015-04-24 18:50:54 -0400

    CurrentWeather.java

---
+++ app/src/main/java/com/mrweaver29/weatherduet/CurrentWeather.java
@@ -0,0 +1,70 @@
+package com.mrweaver29.weatherduet;
+
+/**
+ * Created by Benjamin on 4/24/2015.
+ */
+public class CurrentWeather {
+    private String mLocation;
+    private String mIcon;
+    private long mTime;
+    private double mTemperature;
+    private double mHumidity;
+    private double mPrecipitation;
+    private String mSummary;
+
+    public String getLocation() {
+        return mLocation;
+    }
+
+    public void setLocation(String location) {
+        mLocation = location;
+    }
+
+    public String getIcon() {
+        return mIcon;
+    }
+
+    public void setIcon(String icon) {
+        mIcon = icon;
+    }
+
+    public long getTime() {
+        return mTime;
+    }
+
+    public void setTime(long time) {
+        mTime = time;
+    }
+
+    public double getTemperature() {
+        return mTemperature;
+    }
+
+    public void setTemperature(double temperature) {
+        mTemperature = temperature;
+    }
+
+    public double getHumidity() {
+        return mHumidity;
+    }
+
+    public void setHumidity(double humidity) {
+        mHumidity = humidity;
+    }
+
+    public double getPrecipitation() {
+        return mPrecipitation;
+    }
+
+    public void setPrecipitation(double precipitation) {
+        mPrecipitation = precipitation;
+    }
+
+    public String getSummary() {
+        return mSummary;
+    }
+
+    public void setSummary(String summary) {
+        mSummary = summary;
+    }
+}
