f205d5b4d6b54c67e0c71fa01d7afa48340d0793
mrweaver29 <mrweaver29@outlook.com>
2015-04-25 04:27:55 -0400

    colorwheel and location aware

---
+++ app/src/main/java/com/mrweaver29/weatherduet/ColorWheel.java
@@ -0,0 +1,55 @@
+package com.mrweaver29.weatherduet;
+
+import android.graphics.Color;
+
+import java.util.Random;
+
+/**
+ * Created by Benjamin on 4/25/2015.
+ */
+public class ColorWheel {
+
+    int lastRandomNumber;
+
+    // Member variables (properties about the object)
+    public String [] mColors = {
+            "#39add1", // light blue
+            "#3079ab", // dark blue
+            "#c25975", // mauve
+            "#e15258", // red
+            "#f9845b", // orange
+            "#838cc7", // lavender
+            "#7d669e", // purple
+            "#53bbb4", // aqua
+            "#51b46d", // green
+            "#e0ab18", // mustard
+            "#f092b0", // pink
+    };
+
+    Random randomGenerator = new Random();
+
+    int generateRandom() {
+        int randomNumber = randomGenerator.nextInt(mColors.length);
+        while (randomNumber == lastRandomNumber) {
+            randomNumber = randomGenerator.nextInt(mColors.length);
+        }
+        lastRandomNumber = randomNumber;
+        return randomNumber | lastRandomNumber;
+
+    }
+
+    // Method (abilities: things the object can do)
+    public int getColor() {
+
+        String color;
+
+        // Randomly select a fact
+        //Random randomGenerator = new Random();
+        //int randomNumber = randomGenerator.nextInt(mColors.length);
+
+        color = mColors[generateRandom()];
+        int colorAsInt = Color.parseColor(color);
+
+        return colorAsInt;
+    }
+}
--- app/src/main/java/com/mrweaver29/weatherduet/MainActivity.java
+++ app/src/main/java/com/mrweaver29/weatherduet/MainActivity.java
@@ -1,11 +1,22 @@
 package com.mrweaver29.weatherduet;
 
 import android.content.Context;
+import android.graphics.drawable.Drawable;
+import android.location.Address;
+import android.location.Criteria;
+import android.location.Geocoder;
+import android.location.Location;
+import android.location.LocationManager;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
 import android.os.Bundle;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.ActionBarActivity;
 import android.util.Log;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.squareup.okhttp.Call;
@@ -18,24 +29,79 @@ import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.io.IOException;
+import java.util.List;
+
+import butterknife.ButterKnife;
+import butterknife.InjectView;
 
 
-public class MainActivity extends ActionBarActivity {
+public class MainActivity extends ActionBarActivity  {
 
     public static final String TAG = MainActivity.class.getSimpleName();
 
+    private ColorWheel mColorWheel = new ColorWheel();
+
     private CurrentWeather mCurrentWeather;
+
+    @InjectView(R.id.timeLabel) TextView mTimeLabel;
+    @InjectView(R.id.temperatureLabel) TextView mTemperatureLabel;
+    @InjectView(R.id.humidityValue) TextView mHumidityValue;
+    @InjectView(R.id.precipValue) TextView mPrecipValue;
+    @InjectView(R.id.summaryLabel) TextView mSummaryLabel;
+    @InjectView(R.id.iconImageView) ImageView mIconImageView;
+    @InjectView(R.id.refreshImageView) ImageView mRefreshImageView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.inject(this);
 
+        final RelativeLayout relativeLayout = (RelativeLayout) findViewById(R.id.relativeLayout);
+
+        mRefreshImageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int color = mColorWheel.getColor();
+                relativeLayout.setBackgroundColor(color);
+                getForecast();
+            }
+        });
+
+        getForecast();
+
+    }
+
+    private void getForecast() {
         String apiKey = "";
-        double latitude = 37.8267;
-        double longitude = -122.423;
-        String forecastURL = "https://api.forecast.io/forecast/" +
-        apiKey + "/" + latitude + "," + longitude;
+
+        Geocoder geocoder;
+        String bestProvider;
+        List<Address> user = null;
+        double latitude = 0;
+        double longitude = 0;
+
+        LocationManager lm = (LocationManager) this.getSystemService(Context.LOCATION_SERVICE);
+
+        Criteria criteria = new Criteria();
+        bestProvider = lm.getBestProvider(criteria, false);
+        Location location = lm.getLastKnownLocation(bestProvider);
+
+        if (location == null){
+            Toast.makeText(this, "Location Not found", Toast.LENGTH_LONG).show();
+        }else{
+            geocoder = new Geocoder(this);
+            try {
+                user = geocoder.getFromLocation(location.getLatitude(), location.getLongitude(), 1);
+                latitude=(double)user.get(0).getLatitude();
+                longitude=(double)user.get(0).getLongitude();
+
+            }catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+
+        String forecastURL = String.format("https://api.forecast.io/forecast/%s/%s,%s", apiKey, latitude, longitude);
 
         if (isNetworkAvailable()) {
             OkHttpClient client = new OkHttpClient();
@@ -57,6 +123,12 @@ public class MainActivity extends ActionBarActivity {
                         Log.v(TAG, jsonData);
                         if (response.isSuccessful()) {
                             mCurrentWeather = getCurrentDetails(jsonData);
+                            runOnUiThread(new Runnable() {
+                                @Override
+                                public void run() {
+                                    updateDisplay();
+                                }
+                            });
                         } else {
                             alertUserAboutError();
                         }
@@ -71,7 +143,17 @@ public class MainActivity extends ActionBarActivity {
             Toast.makeText(this, getString(R.string.network_unavailable_message),
                     Toast.LENGTH_LONG).show();
         }
+    }
 
+    private void updateDisplay() {
+        mTemperatureLabel.setText(mCurrentWeather.getTemperature() + "");
+        mTimeLabel.setText("At " + mCurrentWeather.getFormattedTime() + " it is");
+        mHumidityValue.setText(mCurrentWeather.getHumidity() + "");
+        mPrecipValue.setText(mCurrentWeather.getPrecipChance() + "%");
+        mSummaryLabel.setText(mCurrentWeather.getSummary());
+
+        Drawable drawable = ContextCompat.getDrawable(this, mCurrentWeather.getIconId());
+        mIconImageView.setImageDrawable(drawable);
     }
 
     private CurrentWeather getCurrentDetails(String jsonData) throws JSONException {
@@ -79,7 +161,6 @@ public class MainActivity extends ActionBarActivity {
         String timezone = forecast.getString("timezone");
 
         JSONObject currently = forecast.getJSONObject("currently");
-
         CurrentWeather currentWeather = new CurrentWeather();
         currentWeather.setHumidity(currently.getDouble("humidity"));
         currentWeather.setTime(currently.getLong("time"));
@@ -88,9 +169,6 @@ public class MainActivity extends ActionBarActivity {
         currentWeather.setSummary(currently.getString("summary"));
         currentWeather.setTemperature(currently.getDouble("temperature"));
         currentWeather.setTimeZone(timezone);
-
-        Log.d(TAG, currentWeather.getFormattedTime());
-
         return currentWeather;
     }
 
